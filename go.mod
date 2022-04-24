ebf733ade64b4b93016cd1864d28a2a2dbc29f7e
Kegan McIlwaine <kmcilwai@uwyo.edu>
2022-04-24 11:47:07 -0600

    Add endpoints to website.

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/JayCBishop/steganography-website
 go 1.17
 
 require github.com/spf13/pflag v1.0.5
+
+require github.com/gorilla/mux v1.8.0
--- main/main.go
+++ main/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"io/ioutil"
+	"net/http"
 	"os"
-	"strconv"
 
 	"github.com/JayCBishop/steganography-website/internal"
 	"github.com/JayCBishop/steganography-website/models"
+	"github.com/gorilla/mux"
 	"github.com/spf13/pflag"
 )
 
@@ -17,52 +18,52 @@ var (
 	png   internal.MetaChunk
 )
 
-func init() {
-	flags.StringVarP(&opts.Input, "input", "i", "", "Path to the original image file")
-	flags.StringVarP(&opts.Output, "output", "o", "", "Path to output the new image file")
-	flags.BoolVarP(&opts.Meta, "meta", "m", false, "Display the actual image meta details")
-	flags.BoolVarP(&opts.Suppress, "suppress", "s", false, "Suppress the chunk hex data (can be large)")
-	flags.StringVar(&opts.Offset, "offset", "", "The offset location to initiate data injection")
-	flags.BoolVar(&opts.Inject, "inject", false, "Enable this to inject data at the offset location specified")
-	flags.StringVar(&opts.Payload, "payload", "", "Payload is data that will be read as a byte stream")
-	flags.StringVar(&opts.Type, "type", "rNDm", "Type is the name of the Chunk header to inject")
-	flags.StringVar(&opts.Key, "key", "", "The enryption key for payload")
-	flags.BoolVar(&opts.Encode, "encode", false, "XOR encode the payload")
-	flags.BoolVar(&opts.Decode, "decode", false, "XOR decode the payload")
-	flags.Lookup("type").NoOptDefVal = "rNDm"
-	flags.Usage = usage
-	flags.Parse(os.Args[1:])
+// func init() {
+// 	flags.StringVarP(&opts.Input, "input", "i", "", "Path to the original image file")
+// 	flags.StringVarP(&opts.Output, "output", "o", "", "Path to output the new image file")
+// 	flags.BoolVarP(&opts.Meta, "meta", "m", false, "Display the actual image meta details")
+// 	flags.BoolVarP(&opts.Suppress, "suppress", "s", false, "Suppress the chunk hex data (can be large)")
+// 	flags.StringVar(&opts.Offset, "offset", "", "The offset location to initiate data injection")
+// 	flags.BoolVar(&opts.Inject, "inject", false, "Enable this to inject data at the offset location specified")
+// 	flags.StringVar(&opts.Payload, "payload", "", "Payload is data that will be read as a byte stream")
+// 	flags.StringVar(&opts.Type, "type", "rNDm", "Type is the name of the Chunk header to inject")
+// 	flags.StringVar(&opts.Key, "key", "", "The enryption key for payload")
+// 	flags.BoolVar(&opts.Encode, "encode", false, "XOR encode the payload")
+// 	flags.BoolVar(&opts.Decode, "decode", false, "XOR decode the payload")
+// 	flags.Lookup("type").NoOptDefVal = "rNDm"
+// 	flags.Usage = usage
+// 	flags.Parse(os.Args[1:])
 
-	if flags.NFlag() == 0 {
-		flags.PrintDefaults()
-		os.Exit(1)
-	}
-	if opts.Input == "" {
-		log.Fatal("Fatal: The --input flag is required")
-	}
-	if opts.Offset != "" {
-		byteOffset, _ := strconv.ParseInt(opts.Offset, 0, 64)
-		opts.Offset = strconv.FormatInt(byteOffset, 10)
-	}
-	if opts.Suppress && (!opts.Meta) {
-		log.Fatal("Fatal: The --meta flag is required when using --suppress")
-	}
-	if opts.Meta && (opts.Offset != "") {
-		log.Fatal("Fatal: The --meta flag is mutually exclusive with --offset")
-	}
-	if opts.Inject && (opts.Offset == "") {
-		log.Fatal("Fatal: The --offset flag is required when using --inject")
-	}
-	if opts.Inject && (opts.Payload == "") {
-		log.Fatal("Fatal: The --payload flag is required when using --inject")
-	}
-	if opts.Inject && opts.Key == "" {
-		fmt.Println("Warning: No key provided. Payload will not be encrypted")
-	}
-	if opts.Encode && opts.Key == "" {
-		log.Fatal("Fatal: The --encode flag requires a --key value")
-	}
-}
+// 	if flags.NFlag() == 0 {
+// 		flags.PrintDefaults()
+// 		os.Exit(1)
+// 	}
+// 	if opts.Input == "" {
+// 		log.Fatal("Fatal: The --input flag is required")
+// 	}
+// 	if opts.Offset != "" {
+// 		byteOffset, _ := strconv.ParseInt(opts.Offset, 0, 64)
+// 		opts.Offset = strconv.FormatInt(byteOffset, 10)
+// 	}
+// 	if opts.Suppress && (!opts.Meta) {
+// 		log.Fatal("Fatal: The --meta flag is required when using --suppress")
+// 	}
+// 	if opts.Meta && (opts.Offset != "") {
+// 		log.Fatal("Fatal: The --meta flag is mutually exclusive with --offset")
+// 	}
+// 	if opts.Inject && (opts.Offset == "") {
+// 		log.Fatal("Fatal: The --offset flag is required when using --inject")
+// 	}
+// 	if opts.Inject && (opts.Payload == "") {
+// 		log.Fatal("Fatal: The --payload flag is required when using --inject")
+// 	}
+// 	if opts.Inject && opts.Key == "" {
+// 		fmt.Println("Warning: No key provided. Payload will not be encrypted")
+// 	}
+// 	if opts.Encode && opts.Key == "" {
+// 		log.Fatal("Fatal: The --encode flag requires a --key value")
+// 	}
+// }
 
 func usage() {
 	fmt.Fprintf(os.Stderr, "Example Usage: %s -i in.png -o out.png --inject --offset 0x85258 --payload 1234\n", os.Args[0])
@@ -74,11 +75,29 @@ func usage() {
 }
 
 func main() {
-	dat, _ := os.Open(opts.Input)
-	defer dat.Close()
-	bReader, err := internal.PreProcessImage(dat)
+	// dat, _ := os.Open(opts.Input)
+	// defer dat.Close()
+	// bReader, err := internal.PreProcessImage(dat)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// png.ProcessImage(bReader, &opts)
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", mainHandler)
+	http.Handle("/", router)
+
+	//start and listen to requests
+	http.ListenAndServe(":8080", router)
+
+}
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	fileBytes, err := ioutil.ReadFile("index.html")
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	png.ProcessImage(bReader, &opts)
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(fileBytes)
 }
